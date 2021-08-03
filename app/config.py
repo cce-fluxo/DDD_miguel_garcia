1552f408655e59249e10027e2a1e34a92e38943b
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-08-03 15:12:47 -0300

    Deploy iniciado

--- app/config.py
+++ app/config.py
@@ -1,6 +1,13 @@
+from os import environ
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data-dev.db'
+    SECRET_KEY = environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
+    #DATABASE_URI = 'postgresql://nxbmihmxeodwag:7eb0e748ccb1d4f4ea32f3cd5e1f7d9b853b3b09dae76f7d35a630358a28b3ba@ec2-44-194-112-166.compute-1.amazonaws.com:5432/d8u4vvf6i90jfn'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
-    JWT_SECRET_KEY = "hjh32r934h7ry3grjwnef92"
+
+    JWT_SECRET_KEY = environ.get('SECRET_KEY')
+
+    DEBUG = True
     
