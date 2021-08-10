08c4f56dba03bc420fa717161cbed227fb3bcd0c
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-08-10 13:57:38 -0300

    schema

--- app/medico/schema.py
+++ app/medico/schema.py
@@ -12,6 +12,7 @@ class MedicoSchema(ma.SQLAlchemySchema):
     id = ma.Integer(dump_only=True)
     nome=ma.String(required=True)
     cpf=ma.String(required=True)
-    data_de_nascimento=ma.String(required=True)
+    crm=ma.String(required=True)
+    especialidade=ma.String(required=True)
     email=ma.Email(required=True)
     senha = ma.String(Load_only=True, required=True)
