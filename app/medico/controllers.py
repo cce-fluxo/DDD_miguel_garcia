522e3cbb8ed03aebcda6aeff3d4b97fa40400aa3
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-08-10 14:28:54 -0300

    schema

--- app/medico/controllers.py
+++ app/medico/controllers.py
@@ -1,12 +1,13 @@
 from flask.wrappers import Request
 from app.medico.model import Medico
-from flask import request, jsonify
+from flask import request, jsonify, abort,make_response
 from app.extensions import db
 from flask.views import MethodView 
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from app.medico.schema import MedicoSchema
 from app.filters import filter
 import bcrypt
+from sqlalchemy import exc
 
 
 
@@ -23,12 +24,29 @@ class MedicosCreate (MethodView):
         dados = request.json 
         schema = MedicoSchema()
         medico = schema.load(dados)
-        medico.save()
+
+        db.session.add(medico)
+        try:
+            db.session.commit()
+        except exc.IntegrityError as err:
+            db.session.rollback()
+            abort(
+                make_response(jsonify({'errors':str(err.orig)},400)))
+        
+
+
+
+
+
+
+
         return schema.dump(medico),200
 
 class MedicosDetails(MethodView): #/medico/details/<int:id>
     decorators = [jwt_required()]
     def get(self, id):
+        if (get_jwt_identity() != id):
+            return {'error':'Usuario não permitido'}, 400       
         schema = filter.getSchema(
             qs=request.args,schema_cls=MedicoSchema)
         
@@ -36,30 +54,46 @@ class MedicosDetails(MethodView): #/medico/details/<int:id>
         return schema.dump(medico),200
 
     def put(self, id):
-
+        if (get_jwt_identity() != id):
+            return {'error':'Usuario não permitido'}, 400
 
         medico = Medico.query.get_or_404(id)
         schema = MedicoSchema()
         medico = schema.load(request.json,instance=medico)
 
-        medico.save()
+        db.session.add(medico)
+        try:
+            db.session.commit()
+        except exc.IntegrityError as err:
+            db.session.rollback()
+            abort(
+                make_response(jsonify({'errors':str(err.orig)},400)))
         return schema.dump(medico),200
 
     def patch(self, id):
-
+        if (get_jwt_identity() != id):
+            return {'error':'Usuario não permitido'}, 400
         medico = Medico.query.get_or_404(id)
         schema = MedicoSchema()
         medico = schema.load(request.json, instance=medico, partial = True)
 
-        medico.save()
+        db.session.add(medico)
+        try:
+            db.session.commit()
+        except exc.IntegrityError as err:
+            db.session.rollback()
+            abort(
+                make_response(jsonify({'errors':str(err.orig)},400)))
         return schema.dump(medico),200
 
 
 
     def delete(self, id):
-
+        if (get_jwt_identity() != id):
+            return {'error':'Usuario não permitido'}, 400
         medico= Medico.query.get_or_404(id)
-        medico.delete(medico)
+        db.session.delete(medico)
+        db.session.commit()
         return {}, 204
 
 class MedicoLogin(MethodView):
