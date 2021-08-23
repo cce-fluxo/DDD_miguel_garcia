16139381a48a1622359f64b35e4cf41fa9bb6a63
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-08-23 15:08:52 -0300

    CORS added

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from app.config import Config
-from app.extensions import db, migrate, jwt, ma
+from app.extensions import db, migrate, jwt, ma, cors
 from app.medico.routes import medico_api
 from app.paciente.routes import paciente_api
 from app.storages.routes import storage_api
@@ -12,7 +12,9 @@ def create_app():
     migrate.init_app(app,db)
     jwt.init_app(app)
     ma.init_app(app)
+    cors.init_app(app, resources = {r"/":{"origins":""}})
     app.register_blueprint(medico_api)
     app.register_blueprint(paciente_api)
     app.register_blueprint(storage_api)
+    
     return app
