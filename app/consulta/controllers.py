d86df95d5f84b42332012a58a07c50342a2e09a5
Miguel Garcia <72623443+migueldpgarcia@users.noreply.github.com>
2021-08-24 23:00:24 -0300

    consulta.controllers.py minor change in requirement POST

--- app/consulta/controllers.py
+++ app/consulta/controllers.py
@@ -24,7 +24,7 @@ class ConsultaPost(MethodView): #/paciente/create
 
         schema.paciente = paciente
         schema.medico= medico
-        medico.pacientes.append(paciente)
+        
 
         consulta = schema.load(dados)
         consulta.save()
